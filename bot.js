20acdc4df1f5d6c937b94881052bac9d2485185b
Numb4r <yuridimitre@protonmail.com>
2021-07-05 13:20:59 -0300

    corrigido alguns problemas no arquivo de conf bot

--- bot.js
+++ bot.js
@@ -1,26 +1,23 @@
 const Rive = require("rivescript")
 const bot = new Rive({
-    utf8:true,
-    erros:{
+    utf8: true,
+    errors:{
         replyNotFound:"Eu nao tenho resposta para isso"
     }
 })
 
-bot.unicodePunctuation = new RegExp(/[.,!?;:]/g);    
+// bot.unicodePunctuation = new RegExp(/[.,!?;:]/g);    
 bot.loadDirectory("rivescripts").then(loading_done).catch(loading_error)
 function loading_error(error,filename,lineno){
     console.log("Error: "+error)
 }
 function loading_done() {
-    console.log("bot is load")
+    console.log("bot is loading")
     bot.sortReplies()
     let username = "user"
-    bot.reply(username, "Hello, bot!").then(function(reply) {
+    bot.reply(username, "request").then(function(reply) {
         console.log("The bot says: " + reply);
     });
-    bot.reply(username,"Ola").then((reply)=>{
-        console.log(reply)
-    })
 }
 
 module.exports = bot
